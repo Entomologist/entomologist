cfe77ac12b6c7f3e524f245227da44948f1e218c
Matt Barringer <mbarringer@suse.de>
2011-06-08 16:26:14 +0200

    add the translations db copying to the mac deploy script

--- buildmacdmg.sh
+++ buildmacdmg.sh
@@ -5,4 +5,5 @@ make clean
 make
 cp -r translations entomologist.app/Contents/translations
 cp entomologist_*.qm entomologist.app/Contents/translations
+cp entomologist.translations.db entomologist.app/Contents/translations
 macdeployqt entomologist.app -dmg
